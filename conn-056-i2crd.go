401aed269660c88fd9615f1c8d214d5abdd067a0
Damien <maitredede@gmail.com>
2023-08-30 08:35:55 +1100

    feat(i2c): read/write device

---
+++ conn-056-i2crd.go
@@ -0,0 +1,15 @@
+package pigpiod
+
+// I2CRD i2c Read device
+// https://abyz.me.uk/rpi/pigpio/pigs.html#I2CRD
+func (c *Conn) I2CRD(handle uint32, num uint32) ([]byte, error) {
+	cmd := Cmd{
+		cmd: 56,
+		p1:  handle,
+	}
+	res, err := cmd.ExecuteResData(c.tcp)
+	if err != nil {
+		return nil, err
+	}
+	return res.data[:res.p3], nil
+}
---
+++ conn-057-i2cwd.go
@@ -0,0 +1,17 @@
+package pigpiod
+
+// I2CWD i2c Write device
+// https://abyz.me.uk/rpi/pigpio/pigs.html#I2CWD
+func (c *Conn) I2CWD(handle uint32, bvs []byte) error {
+	cmd := Cmd{
+		cmd:  57,
+		p1:   handle,
+		p3:   uint32(len(bvs)),
+		data: bvs,
+	}
+	/*res*/ _, err := cmd.ExecuteRes(c.tcp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
